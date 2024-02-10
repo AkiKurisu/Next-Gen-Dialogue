33c013d4dbf41c8486fffef0203e0b27a57a06dc
AkiKurisu <2683987717@qq.com>
2024-02-10 15:41:25 +0800

    Update AITurboSetting.cs
    Public setting fields

--- NGDS/Runtime/Model/AI/AITurboSetting.cs
+++ NGDS/Runtime/Model/AI/AITurboSetting.cs
@@ -4,28 +4,28 @@ namespace Kurisu.NGDS.AI
         [CreateAssetMenu(fileName = "AITurboSetting", menuName = "Next Gen Dialogue/AITurboSetting")]
         public class AITurboSetting : ScriptableObject
         {
-                private enum GPT_APIMode
+                public enum GPT_APIMode
                 {
                         Chat, Completions
                 }
-                private enum GPT_ModelType
+                public enum GPT_ModelType
                 {
                         [InspectorName("GPT-3.5-Turbo")]
                         GPT3_5,
                         [InspectorName("GPT-4")]
                         GPT4
                 }
-                private enum VITS_ModelType
+                public enum VITS_ModelType
                 {
                         VITS, BertVITS2
                 }
                 [field: Header("LLM Setting")]
                 [field: SerializeField]
-                public string ChatGPT_URL_Override { get; private set; }
-                [SerializeField, Tooltip("Whether use GPT Chat Mode or Completions Mode, Chat Mode is better when only has two role")]
-                private GPT_APIMode gpt_APIMode;
-                [SerializeField, Tooltip("Set ChatGPT model type")]
-                private GPT_ModelType gptType;
+                public string ChatGPT_URL_Override { get; set; }
+                [Tooltip("Whether use GPT Chat Mode or Completions Mode, Chat Mode is better when only has two role")]
+                public GPT_APIMode gpt_APIMode;
+                [Tooltip("Set ChatGPT model type")]
+                public GPT_ModelType gptType;
                 public string GPT_Model
                 {
                         get
@@ -36,24 +36,24 @@ namespace Kurisu.NGDS.AI
                 }
                 public bool ChatMode => gpt_APIMode == GPT_APIMode.Chat;
                 [field: SerializeField]
-                public string OpenAIKey { get; private set; }
+                public string OpenAIKey { get; set; }
                 [field: SerializeField]
-                public string LLM_Address { get; private set; } = "127.0.0.1";
+                public string LLM_Address { get; set; } = "127.0.0.1";
                 [field: SerializeField]
-                public string LLM_Port { get; private set; } = "5001";
+                public string LLM_Port { get; set; } = "5001";
                 [field: Header("Translation Setting")]
                 [field: SerializeField, Tooltip("Enable this to use auto google translation before LLM input")]
-                public bool Enable_GoogleTranslation { get; private set; }
+                public bool Enable_GoogleTranslation { get; set; }
                 [field: SerializeField, Tooltip("LLM output and input language code")]
-                public string LLM_Language { get; private set; } = "en";
+                public string LLM_Language { get; set; } = "en";
 #if NGD_USE_VITS
                 [field: SerializeField, Tooltip("VITS input language code")]
-                public string VITS_Language { get; private set; } = "ja";
+                public string VITS_Language { get; set; } = "ja";
                 [field: Header("VITS Setting (Experimental)")]
                 [field: SerializeField]
-                public string VITS_Address { get; private set; } = "127.0.0.1";
-                [SerializeField, Tooltip("Set vits model type")]
-                private VITS_ModelType vitsType;
+                public string VITS_Address { get; set; } = "127.0.0.1";
+                [Tooltip("Set vits model type")]
+                public VITS_ModelType vitsType;
                 public string VITS_Model
                 {
                         get
@@ -63,11 +63,11 @@ namespace Kurisu.NGDS.AI
                         }
                 }
                 [field: SerializeField]
-                public string VITS_Port { get; private set; } = "23456";
+                public string VITS_Port { get; set; } = "23456";
                 [field: SerializeField, Tooltip("Specify the language, leave empty to use auto mode.")]
-                public string VITS_Lang { get; private set; } = string.Empty;
+                public string VITS_Lang { get; set; } = string.Empty;
                 [field: SerializeField, Tooltip("Specify the length of output which effects the speed of speaking.")]
-                public string VITS_Length { get; private set; } = string.Empty;
+                public string VITS_Length { get; set; } = string.Empty;
 #endif
         }
 }
