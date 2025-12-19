05da93a15aec08e8cdbfed7e2c544773389ad8a1
AkiKurisu <2683987717@qq.com>
2025-12-19 23:18:21 +0800

    Making CompositeNode and ActionNode Obsoleted

--- Runtime/Graph/Nodes/ActionNode.cs
+++ Runtime/Graph/Nodes/ActionNode.cs
@@ -1,5 +1,8 @@
+using System;
+
 namespace NextGenDialogue.Graph
 {
+    [Obsolete("ActionNode is no longer used, use Flow instead.")]
     public abstract class ActionNode : DialogueNode
     {
     }
